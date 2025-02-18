8c951bb104cf4a61ab6ac6eaa374a357a76c1bb6
nyel-101 <dandevjr@gmail.com>
2025-02-18 07:32:17 +0000

    refactor(ui): update caching layer

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea3f73868cf3438488f3f412b8415645 = () => {
-  return '2026-06-02T06:07:46Z';
+const node_2bf5f0d060c54986bb171dc8169d8e5e = () => {
+  return '2025-02-18T07:32:17Z';
 };
