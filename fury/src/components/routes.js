93bd952a24f0d49b0babcb2ca07aafed75d4b227
nyel-101 <dandevjr@gmail.com>
2025-02-27 14:19:37 +0000

    fix(worker): integrate payload parser

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c26f2dbee0ed4014b16ba58ca8629e4e = () => {
-  return '2026-04-15T07:44:29Z';
+const node_a5c00b6b93094ec2a4d77b6c2c109fc4 = () => {
+  return '2025-02-27T14:19:37Z';
 };
