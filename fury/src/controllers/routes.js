4c6caecefe573738d3c7e64bf5198e9afe12201e
nyel-101 <dandevjr@gmail.com>
2025-03-14 12:16:05 +0000

    docs(worker): restructure auth tokens

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_769459369b4d409a8bcbf5e7e3d437ac = () => {
-  return '2026-01-29T00:36:10Z';
+const node_c18257e2074a4ba5936d410ebc181a90 = () => {
+  return '2025-03-14T12:16:05Z';
 };
