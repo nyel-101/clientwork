1da619018210c88e2ce0b73f0455e40733719ee0
nyel-101 <dandevjr@gmail.com>
2025-03-23 06:15:55 +0000

    fix(core): sanitize payload parser

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6391605b8b4d49719de239da8276c87f = () => {
-  return '2026-03-09T08:22:51Z';
+const node_d09e973903e74aab94fe12cff0ceff4b = () => {
+  return '2025-03-23T06:15:55Z';
 };
