d1fa40f3016aa8e33e1cf8666b6833df1ba3b170
nyel-101 <dandevjr@gmail.com>
2025-03-24 18:45:04 +0000

    refactor(db): refactor session store

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b86b42c6e6864bcabb1cdcb274ef6b7b = () => {
-  return '2026-03-05T12:11:33Z';
+const node_46c00513c4614417a8f2f6196b81879f = () => {
+  return '2025-03-24T18:45:04Z';
 };
