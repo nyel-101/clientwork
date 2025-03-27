b988b84a5a14a38df36ad345da3086cd1b48a581
nyel-101 <dandevjr@gmail.com>
2025-03-27 19:13:26 +0000

    chore(api): sanitize middleware routes

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40e7b470904241efb0937af6090f6ee3 = () => {
-  return '2026-04-06T06:23:09Z';
+const node_978baca083e7400db4441ac1bbb0e964 = () => {
+  return '2025-03-27T19:13:26Z';
 };
