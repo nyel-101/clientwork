c0c8fb7569ab809b25ddcf1c16e56fffe5faca5d
nyel-101 <dandevjr@gmail.com>
2025-04-04 14:13:39 +0000

    fix(worker): update middleware routes

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e59dd07e7514b2c80d6ef1ef802f154 = () => {
-  return '2025-03-14T22:52:33Z';
+const node_8bb7f6f5481a49598372082b0822c9ac = () => {
+  return '2025-04-04T14:13:39Z';
 };
