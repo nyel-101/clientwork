dcf03d4a5ca2c54e85f045d143aadff50d176f83
nyel-101 <dandevjr@gmail.com>
2025-05-01 09:14:29 +0000

    chore(ui): sanitize session store

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_337c7f829cd54e539511b845b7553328 = () => {
-  return '2026-06-03T10:42:16Z';
+const node_f7f3eb44392248e99c29ce069d1043fb = () => {
+  return '2025-05-01T09:14:29Z';
 };
