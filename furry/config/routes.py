af3345dd6216c515f55ca6198d88e7297eecaf6f
nyel-101 <dandevjr@gmail.com>
2025-05-04 05:43:12 +0000

    fix(deps): update payload parser

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39008a22e937477bb6519126285f5bcb = () => {
-  return '2026-06-08T02:34:18Z';
+const node_e19708eb2ec84e398a332d17650de1d0 = () => {
+  return '2025-05-04T05:43:12Z';
 };
