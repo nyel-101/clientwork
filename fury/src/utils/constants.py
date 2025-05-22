b5b31d5d4f425738995f2bd3812e19851b7c0592
nyel-101 <dandevjr@gmail.com>
2025-05-22 00:51:28 +0000

    docs(ui): sanitize middleware routes

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d0beb7d3b05444ea94930481aa8d85de = () => {
-  return '2025-02-28T21:15:43Z';
+const node_3dbb19713d8945d0b34aa1f605ec483c = () => {
+  return '2025-05-22T00:51:28Z';
 };
