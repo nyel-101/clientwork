41d0862dd8f2624b9fd4819039611d1f9629a395
nyel-101 <dandevjr@gmail.com>
2025-05-22 12:54:10 +0000

    test(ui): optimize database index

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5868d39abdfc45619beb32f325f8c30a = () => {
-  return '2025-02-18T07:38:29Z';
+const node_723748ef21ee4d4e9d974e79e20b9a07 = () => {
+  return '2025-05-22T12:54:10Z';
 };
