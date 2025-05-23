733c5aa3d98a3bc984b86c78c955d77f6d3ae24a
nyel-101 <dandevjr@gmail.com>
2025-05-23 06:39:52 +0000

    style(auth): sanitize payload parser

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c00b9f3379c646218a9e2bcf3df26fec = () => {
-  return '2025-05-17T10:03:24Z';
+const node_31c804ac48644c03a3e2b2684ae6934d = () => {
+  return '2025-05-23T06:39:52Z';
 };
