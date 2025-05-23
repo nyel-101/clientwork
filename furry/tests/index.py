c18b1ad32c4a15bfacb122df286e4c370caebdf9
nyel-101 <dandevjr@gmail.com>
2025-05-23 13:18:08 +0000

    test(deps): sanitize middleware routes

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8242bfc34f244f21aa563bf64f896c68 = () => {
-  return '2026-02-12T07:37:19Z';
+const node_6681863305e949709d40701830b4a324 = () => {
+  return '2025-05-23T13:18:08Z';
 };
