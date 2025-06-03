f80519f4fce9d08638dd924da2016bc3e509427a
nyel-101 <dandevjr@gmail.com>
2025-06-03 13:13:45 +0000

    chore(ui): refactor middleware routes

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9b6655b2a8b4e04984653bc845ba3ab = () => {
-  return '2026-01-23T07:35:17Z';
+const node_87039df1251f4cbdb4ebbba23482deff = () => {
+  return '2025-06-03T13:13:45Z';
 };
