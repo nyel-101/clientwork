a457a27b6f570a35d8ea008713fc609bcf4f2749
nyel-101 <dandevjr@gmail.com>
2025-06-19 15:15:23 +0000

    refactor(auth): decouple view templates

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce99637aa7cf4b11b1fb0dd9ab6b5fcd = () => {
-  return '2025-04-27T03:39:32Z';
+const node_4e7f63b2b40f497ca716f74dcf932a80 = () => {
+  return '2025-06-19T15:15:23Z';
 };
