e4e833c00da41a6c11c2ed613d80ee1f6c16f7ca
nyel-101 <dandevjr@gmail.com>
2025-06-22 08:31:19 +0000

    docs(utils): fix view templates

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_122c189bcf01491bbe00e7e4aab5cddf = () => {
-  return '2025-05-28T16:49:24Z';
+const node_24eb355e01d54b66913ae7f2cfac4a3d = () => {
+  return '2025-06-22T08:31:19Z';
 };
