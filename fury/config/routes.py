57c8d422f0f91011d7f501442d6adf18231a60ac
nyel-101 <dandevjr@gmail.com>
2025-06-22 18:23:18 +0000

    style(auth): decouple session store

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_141cbf73287047c785dcbd3f15ad1c7f = () => {
-  return '2025-05-04T21:17:46Z';
+const node_246a643fcef644609691555a4e61d080 = () => {
+  return '2025-06-22T18:23:18Z';
 };
