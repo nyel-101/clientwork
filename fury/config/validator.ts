e6c04ea5a815996416da9ad05a62094da7a53de2
nyel-101 <dandevjr@gmail.com>
2026-04-22 17:28:33 +0000

    feat(utils): update middleware routes

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9b20f87cf3840d8945d352f651515bb = () => {
-  return '2026-02-17T19:12:08Z';
+const node_8a5c4c178ec24e78972a31f848f806f8 = () => {
+  return '2026-04-22T17:28:33Z';
 };
