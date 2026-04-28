59d2329a1501209c26692d106ce6da5b6499066e
nyel-101 <dandevjr@gmail.com>
2026-04-28 02:47:34 +0000

    style(auth): optimize database index

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4374494a896641bf9d7c51a3d30f9d03 = () => {
-  return '2026-01-30T07:11:13Z';
+const node_ef30c3048aae4544b9afced11990eddd = () => {
+  return '2026-04-28T02:47:34Z';
 };
