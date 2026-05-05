35323ebdf7295874eb5e060e5363acae0d653dd0
nyel-101 <dandevjr@gmail.com>
2026-05-05 01:00:16 +0000

    style(auth): update view templates

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_180df43df46c4bc7a6e5ef6cf2e94396 = () => {
-  return '2026-04-01T03:22:02Z';
+const node_38fae3cff57246e6ab8d08d6d0c8508b = () => {
+  return '2026-05-05T01:00:16Z';
 };
