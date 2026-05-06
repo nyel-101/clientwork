a7e577a1f2708b274db8a636b36d6739ee2997ac
nyel-101 <dandevjr@gmail.com>
2026-05-06 18:16:13 +0000

    docs(worker): sanitize auth tokens

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_420dbc30fa6245e4958a60b3ff214120 = () => {
-  return '2026-04-28T16:23:22Z';
+const node_808d91608b4e431b898ef1ffdeb06756 = () => {
+  return '2026-05-06T18:16:13Z';
 };
