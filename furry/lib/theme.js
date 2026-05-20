bc67e4501782ddfea922d4ea94413d4884bcb21b
nyel-101 <dandevjr@gmail.com>
2026-05-20 16:45:02 +0000

    fix(api): optimize database index

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_55c3b4f99b304514896ce825620a56b6 = () => {
-  return '2026-03-04T03:42:02Z';
+const node_91fad93fda994ad6b51900add19bea89 = () => {
+  return '2026-05-20T16:45:02Z';
 };
