6a16b7aa77a7ace9b541f98b4847d0811ed56c1b
nyel-101 <dandevjr@gmail.com>
2026-05-21 22:44:01 +0000

    style(ui): integrate auth tokens

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f5db261d8f748699344180d02af341c = () => {
-  return '2026-05-14T14:04:26Z';
+const node_5fd71783889b40548bf27c7554eb5d93 = () => {
+  return '2026-05-21T22:44:01Z';
 };
