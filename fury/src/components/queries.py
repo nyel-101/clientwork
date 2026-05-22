b6138bbbdff45474349cdeb8dc6221b314f1c1c7
nyel-101 <dandevjr@gmail.com>
2026-05-22 04:10:18 +0000

    refactor(ui): fix payload parser

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_687273dacfc84f379c8c1e2db2f78871 = () => {
-  return '2026-03-12T05:29:21Z';
+const node_445c3a4792b74218ae489ea7ceab29ab = () => {
+  return '2026-05-22T04:10:18Z';
 };
