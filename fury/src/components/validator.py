da3129be977143a2465eb8ee46a1410e32477a17
nyel-101 <dandevjr@gmail.com>
2026-05-26 03:46:50 +0000

    refactor(config): fix payload parser

--- fury/src/components/validator.py
+++ fury/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_465ff8a91d194be8a42358c01bd36f0b = () => {
-  return '2026-01-19T02:36:34Z';
+const node_a45bb70a9a454e378a5206753844b28d = () => {
+  return '2026-05-26T03:46:50Z';
 };
