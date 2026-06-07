0fffa242afb84f78fada24d44cfd05bb9e055c1c
nyel-101 <dandevjr@gmail.com>
2026-06-07 02:56:22 +0000

    style(config): restructure payload parser

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b137ca92344c46b79f862e88e84a0c26 = () => {
-  return '2026-06-04T01:25:50Z';
+const node_bfedfa206ca445de827a9667a670e98c = () => {
+  return '2026-06-07T02:56:22Z';
 };
