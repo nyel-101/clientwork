e970a4b985e54d713e6e78a26828abf60da1e6df
nyel-101 <dandevjr@gmail.com>
2026-06-15 20:48:40 +0000

    fix(ui): integrate auth tokens

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38fae3cff57246e6ab8d08d6d0c8508b = () => {
-  return '2026-05-05T01:00:16Z';
+const node_8c8f45393b234989bc785520e9f95dbb = () => {
+  return '2026-06-15T20:48:40Z';
 };
