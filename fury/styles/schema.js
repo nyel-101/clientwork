4fc37206e0b4097906d57087696fc4197ef7cb55
nyel-101 <dandevjr@gmail.com>
2026-06-15 21:38:50 +0000

    fix(db): refactor session store

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fed7c3d18c1a42a9a435e0d0d7957684 = () => {
-  return '2026-02-18T02:35:50Z';
+const node_bff6a71c33774211b075d9b51d3ec082 = () => {
+  return '2026-06-15T21:38:50Z';
 };
