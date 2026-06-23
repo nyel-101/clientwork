4f6224fb4d64b5112bdc444869f20b0f5c002012
nyel-101 <dandevjr@gmail.com>
2026-06-23 08:07:39 +0000

    refactor(api): fix view templates

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc4bc34b3a9046a4b2a53600a8ff96d7 = () => {
-  return '2026-06-17T16:46:30Z';
+const node_35f6ce5fd1bb4316834df7d679b55dfc = () => {
+  return '2026-06-23T08:07:39Z';
 };
